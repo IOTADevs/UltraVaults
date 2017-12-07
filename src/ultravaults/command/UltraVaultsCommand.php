c76d938c39da73695393fb1dc882866c45d3b0fe
Az928 <TheAz928@users.noreply.github.com>
2017-12-07 23:57:24 +0600

    Create UltraVaultsCommand.php

---
+++ src/ultravaults/command/UltraVaultsCommand.php
@@ -0,0 +1,95 @@
+<?php
+namespace ultravaults\command;
+
+use pocketmine\Player;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use ultravaults\Core;
+
+/**
+ * Developed by TheAz928 (IOTATeam)
+ * This software is under GPL v3 and later
+ * Modifications to this software is allowed
+ * Under the GPL license
+ */
+
+class UltraVaultsCommand extends Command{
+	
+	/** @var array */
+	private $op_cmds = [
+	  "seevault",
+	  "delvault"
+	];
+	
+	/**
+	 * IDK ;)
+	 */
+	
+	public function __construct(){
+	    parent::__construct("ultravaults", "Open your vaults!", null, ["v", "pv", "uv"]);
+	}
+	
+	/**
+	 * @param CommandSender $player
+	 * @param string $label
+	 * @param array $args
+	 */
+	
+	public function execute(CommandSender $player, string $label, array $args){
+	    if($player instanceof Player == false){
+	       $player->sendMessage(Core::getPrefix()."You cannot use that here!");
+	    return false;
+	    }
+	    if(is_numeric($args[0])){
+		   if(((Int) $args[0] <= Core::get("max.vaults") or $player->hasPermission(Core::get("perm")["open.perm"].$args[0])) and (Int) $args[0] > 0){
+			  $inv = Core::initVault($args[0], $player->getName(), $player);
+			  $player->addWindow($inv);
+			}else{
+			  $player->sendMessage(Core::getPrefix()."§7You don't have permission to open that vault!");
+			}
+		 return true;
+		 }
+	    if($player->hasPermission("vault.administration")){
+		   if(in_array($args[0], $this->op_cmds)){
+			  switch($args[0]){
+			     case "seevault":
+			        if(isset($args[1]) == false or isset($args[2]) == false){
+				       $player->sendMessage(Core::getPrefix()."§7/ultravaults seevault {player name} {vault id}");
+				     return false;
+				     }
+				     $check = Core::getVaultContents((Int) $args[2], $args[1]);
+				     if($check === []){
+					    $player->sendMessage(Core::getPrefix()."§7Vault doesn't exists or empty!");
+					  return false;
+					  }
+					  $inv = Core::initVault((Int) $args[2], $args[1], $player);
+					  $player->addWindow($inv);
+			     break;
+			     case "delvault":
+			       if(isset($args[1]) == false or isset($args[2]) == false){
+				       $player->sendMessage(Core::getPrefix()."/ultravaults delvault {player name} {vault id}");
+				     return false;
+				     }
+				     $check = Core::getVaultContents((Int) $args[2], $args[1]);
+				     if($check === []){
+					    $player->sendMessage(Core::getPrefix()."§7Vault doesn't exists or empty!");
+					  return false;
+					  }
+					  Core::saveVault((Int) $args[2], $args[1], []);
+					  $player->sendMessage(Core::getPrefix()."§7Deleted the vault!");
+			     break;
+			  }
+			}else{
+			  $player->sendMessage(Core::getPrefix()."§7Unknown subcommand! Try, ");
+			  $player->sendMessage("§l§8»§r§7 seevault [player] [vault id] - See [player]'s vault");
+			  $player->sendMessage("§l§8»§r§7 delvault [player] [vault id] - Delete [player]'s vault");
+			}
+		}else{
+			$player->sendMessage(Core::getPrefix()."§7You don't have permission to run this command!");
+     return false;
+		}
+	return true;
+	}
+}
